bd9d2cdf11239a0f6264ec6f7e987ce9f71d4578
gonultasbu <gonultasbu@users.noreply.github.com>
2018-04-22 10:13:14 +0300

    label matching function written
    matches the format between correctly labeled dataset and the original DDD dataset

---
+++ Match_Labels/cmake-build-debug/CMakeFiles/Match_Labels.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Match_Labels.dir/main.cpp.o"
+  "Match_Labels.pdb"
+  "Match_Labels"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Match_Labels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Match_Labels/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void gen_file(ifstream& input_file_param, ofstream& output_file_param){
+    string start="0",stop="0";
+    int c;
+    getline(input_file_param,start);
+    while(getline(input_file_param,start,' ')){
+
+        for(c=stoi(stop);c<=stoi(start);c++){ //generate the zeros
+            output_file_param<<"0";
+        }
+
+        getline(input_file_param,stop);
+        stop.pop_back(); //Get rid of the newline character, remove on DOS while compiling
+
+        for(c=stoi(start);c<=stoi(stop);c++){ //generate the ones
+            output_file_param<<"1";
+        }
+    }
+}
+
+
+int main() {
+    ifstream input_file;
+    input_file.open("sleepyCombination.txt");
+    ofstream output_file;
+    output_file.open("generatedFile.txt");
+
+    gen_file(input_file,output_file);
+
+    output_file.close();
+    input_file.close();
+    return 0;
+}
