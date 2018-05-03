4e3a81d607a2c9b9e1a542194cf901ddde0b3abe
gonultasbu <gonultasbu@users.noreply.github.com>
2018-05-03 15:36:28 +0300

    batch data processing code ready
    give the directory as input, however the process is slow and speed must be increased, maybe written in C++

--- Batch_Processing/traverse_subdirectories.py
+++ Batch_Processing/traverse_subdirectories.py
@@ -1,4 +1,5 @@
 import os
+from video_processor import process_video
 
 def subdir_traverser(input_dir):
     file_count=0;
@@ -6,13 +7,14 @@ def subdir_traverser(input_dir):
         for name in files:
             if (name.endswith("(1).avi")):
                 print(os.path.join(root, name))
-                #function(os.path.join(root,name)
+                process_video(os.path.join(root,name))
                 file_count=file_count+1;
     print (file_count , "VIDEOS ARE PROCESSED")
 
+   #unnecessary part
    #for name in dirs:
-   #  print(os.path.join(root, name))
+   #print(os.path.join(root, name))
 
 
-
-subdir_traverser("/Users/bmg/Dropbox/ITU/2017 BITIRME/Datasets")
+#GIVE THE DIRECTORY YOU WANT TO PROCESS AS INPUT
+subdir_traverser("C:\\Users\\Mert\\Dropbox\\ITU\\2017 BITIRME\\DATASETS")
